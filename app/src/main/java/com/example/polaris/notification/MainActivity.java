40f81224513ace199baf8b6fecf7e616602939ba
polaris <tom9polaris@vip.qq.com>
2016-07-12 00:26:22 +0800

    点击后消息在消息栏消失

--- app/src/main/java/com/example/polaris/notification/MainActivity.java
+++ app/src/main/java/com/example/polaris/notification/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.polaris.notification;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,12 +26,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             switch (v.getId()){
                 case R.id.send_notice:
                     NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+                    Intent intent = new Intent(this,NotificationActivity.class);
+                    PendingIntent pi = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_CANCEL_CURRENT);
                     Notification builder = new Notification.Builder(this).
                             setContentTitle("欧洲杯").
                             setContentText("葡法决赛，葡萄牙夺冠").
                             setSmallIcon(R.drawable.iclauncherweb).
                             setContentInfo("葡萄牙首次夺冠").
-                            setWhen(System.currentTimeMillis()).build();
+                            setWhen(System.currentTimeMillis()).
+                            setContentIntent(pi).     //添加具体信息
+                            build();
+                    builder.flags |= Notification.FLAG_AUTO_CANCEL;    //运行后消失
+
                     manager.notify(1,builder);    //Post a notification to be shown in the status bar.
                     break;
                 default:
---
+++ app/src/main/java/com/example/polaris/notification/NotificationActivity.java
@@ -0,0 +1,15 @@
+package com.example.polaris.notification;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Polaris on 2016/7/12.
+ */
+public class NotificationActivity extends Activity {
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
+}
